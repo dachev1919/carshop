98b4cd22bd691c51e7c5b8fbac541010fc2a0d7c
Dachev <dachev1919@gmail.com>
2023-07-19 16:25:05 +0300

    added hero

---
+++ components/CustomButton.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import {CustomButtonProps} from "@/types";
+
+const CustomButton = ({ title, containerStyles, handleClick, btnType = 'button' }: CustomButtonProps) => {
+  return (
+    <button
+      disabled={ false }
+      type={ btnType }
+      className={`custom-btn ${containerStyles}`}
+      onClick={ handleClick }
+    >
+      <span className="flex-1">
+        { title }
+      </span>
+    </button>
+  );
+};
+
+export default CustomButton;
