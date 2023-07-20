99d5f141eca657c918ffe4f0c7c972f7861737ef
Dachev <dachev1919@gmail.com>
2023-07-20 15:48:13 +0300

    added filters

---
+++ components/CustomFilter.tsx
@@ -0,0 +1,11 @@
+import {CustomFilterProps} from "@/types";
+
+const CustomFilter = ({title}: CustomFilterProps) => {
+  return (
+    <div>
+      Custom filter
+    </div>
+  );
+};
+
+export default CustomFilter;
