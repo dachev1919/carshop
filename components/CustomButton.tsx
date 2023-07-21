e9c4308f868eebf8ece914f5d307e5088291c78d
Dachev <dachev1919@gmail.com>
2023-07-21 15:36:52 +0300

    button addition

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,18 +1,32 @@
 'use client';
 
 import {CustomButtonProps} from "@/types";
+import Image from "next/image";
 
-const CustomButton = ({ title, containerStyles, handleClick, btnType = 'button' }: CustomButtonProps) => {
+const CustomButton = ({
+                        title,
+                        containerStyles,
+                        handleClick,
+                        btnType = 'button',
+                        textStyles = '',
+                        rightIcon,
+                        isDisabled = false
+}: CustomButtonProps) => {
   return (
     <button
-      disabled={ false }
+      disabled={ isDisabled }
       type={ btnType }
       className={`custom-btn ${containerStyles}`}
       onClick={ handleClick }
     >
-      <span className="flex-1">
+      <span className={`flex-1 ${textStyles}`}>
         { title }
       </span>
+      {rightIcon && (
+        <div className='relative w-6 h-6'>
+          <Image src={rightIcon} alt='right icon' fill className='object-contain' />
+        </div>
+      )}
     </button>
   );
 };
