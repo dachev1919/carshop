1a723c96fdebae30d8a0abbc4ef6e891bc9413a5
Dachev <dachev1919@gmail.com>
2023-07-24 16:55:37 +0300

    changes in car card

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { CarProps } from "@/types";
-import { calculateCarRent } from "@/utils";
+import {calculateCarRent, generateCarImageUrl} from "@/utils";
 import Image from "next/image";
 import CustomButton from "@/components/CustomButton";
 import { useState } from "react";
@@ -36,7 +36,7 @@ const CarCard = ({ car }: CarCardProps) => {
       </p>
 
       <div className='relative w-full h-40 my-3 object-contain'>
-        <Image src='/hero.png' alt='car model' fill priority className='object-contain' />
+        <Image src={generateCarImageUrl(car)} alt='car model' fill priority className='object-contain' />
       </div>
 
       <div className='relative flex w-full mt-2'>
