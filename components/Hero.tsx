4d61250ddd00bca0758a364a502f581ade2ec627
Dachev <dachev1919@gmail.com>
2023-07-27 12:20:39 +0300

    minor changes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import Image from 'next/image';
-import {CustomButton} from "@/components";
+import { CustomButton } from "@/components";
+import hero from '../public/hero.png';
 
 const Hero = () => {
   const handleScroll = () => {
@@ -26,7 +27,7 @@ const Hero = () => {
       </div>
       <div className="hero__image-container">
         <div className="hero__image">
-          <Image src='/hero.png' alt='hero' fill className='object-contain' />
+          <Image src={hero} alt='hero' fill className='object-contain' />
         </div>
         <div className="hero__image-overlay"/>
       </div>
